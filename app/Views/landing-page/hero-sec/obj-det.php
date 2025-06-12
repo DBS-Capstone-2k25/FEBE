5659628e45643d7073648b29a216058c2f9a8d79
Ame0thyst <kokoo15011209@gmail.com>
2025-06-12 21:21:24 +0700

    🧋feat : hero, nav

--- app/Views/landing-page/hero-sec/obj-det.php
+++ app/Views/landing-page/hero-sec/obj-det.php
@@ -1,5 +1,5 @@
  <div class="min-w-full relative">
-     <img src="<?= base_url('assets/img/chatbot.webp') ?>" class="absolute inset-0 w-full h-full object-cover" alt="Slide 1">
+     <img src="<?= base_url('assets/img/object-detect.webp') ?>" class="absolute inset-0 w-full h-full object-cover" alt="Slide 1">
      <div class="absolute inset-0 bg-black bg-opacity-50"></div>
      <!-- Content overlay for slide 1 -->
      <div class="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-transparent"
@@ -8,8 +8,8 @@
      <div class="absolute inset-0">
          <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 h-full items-center">
              <div class="mr-auto place-self-center lg:col-span-7">
-                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">Digital Banking Solutions for Everyone</h1>
-                 <p class="max-w-2xl mb-6 font-light text-gray-100 lg:mb-8 md:text-lg lg:text-xl">Transform your banking experience with our innovative digital solutions. Secure, fast, and user-friendly banking at your fingertips.</p>
+                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">Bedah Sampah: Solusi Cerdas untuk Meningkatkan Kesadaran Lingkungan dengan AI dan Teknologi Chatbot</h1>
+                 <p class="max-w-2xl mb-6 font-light text-gray-100 lg:mb-8 md:text-lg lg:text-xl text-justify">Proyek Bedah Sampah hadir dengan tiga fitur utama yang memadukan kecerdasan buatan (AI) dan chatbot interaktif untuk mempermudah pengelolaan sampah..</p>
                  <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                      <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300">
                          Get started
